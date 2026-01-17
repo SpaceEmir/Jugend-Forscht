52d35c42a7bec0bf032ead2cb33156dea4d45c5a
Emir <ahmetemirguzelkokar@gmail.com>
2026-01-17 11:10:29 +0100

    I changed the week graphic to week table.

--- test.py
+++
@@ -1,7 +0,0 @@
-import os
-
-DATEI = "emotionen.json"
-print("Çalışma dizini:", os.getcwd())
-print("Script dizini:", os.path.dirname(os.path.abspath(__file__)))
-print("Aranan dosya:", DATEI)
-print("Dosya mevcut mu?", os.path.exists(DATEI))
