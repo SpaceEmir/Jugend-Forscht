b047782ee0d3c5b9b2928ead0d056899bb155fde
Emir <ahmetemirguzelkokar@gmail.com>
2026-01-17 11:16:05 +0100

    I maked the code shorter.

--- week_table.py
+++ week_table.py
@@ -1,18 +1,12 @@
 import pandas as pd
 from asking import gibTage as gT
 
-data = {
-    "Name": ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"]
-}
-
 tage = gT()
 
-gefuehle = []
-
-for tag in data["Name"]:
-    gefuehle.append(tage[tag][0].capitalize())
-
-data["Gefühl"] = gefuehle
+data = {
+    "Tag": ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"],
+    "Gefühl": [tage[tag][0].capitalize() for tag in ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"]]
+}
 
 df = pd.DataFrame(data)
 print(df)
